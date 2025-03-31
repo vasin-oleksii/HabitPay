8403130bdae488e2cd1704028223c746bdd5d33f
v4s1n <vasin.oleksii@gmail.com>
2025-03-31 15:29:32 +0200

    setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Box, Center, Heading } from "@chakra-ui/react";
+
+const App = () => {
+  return (
+    <Box>
+      <Center>
+        <Heading>Heeey 👋 is HabitPay </Heading>
+      </Center>
+    </Box>
+  );
+};
+
+export default App;
